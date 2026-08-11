3d0386e114dd2c29f24e0fa88fc01f37f8ac539f
Michael Ndiiwda Henok <michaelnd698@gmail.com>
2026-08-11 21:31:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -126,7 +126,7 @@ function createInflationChart() {
                     4.1,
                     4.4,
                     4.5,
-                    4.2
+                    4.3
                 ],
 
                 borderColor: "#0077cc",
